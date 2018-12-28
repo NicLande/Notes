f7056770cfbd19c0bcbbcb1ed77ca16b66b4927e
nic lande <ndl1391@rit.edu>
2018-12-28 16:23:01 -0500

    First Commit

---
+++ app/src/main/java/com/example/r2_l2/notes/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.r2_l2.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
